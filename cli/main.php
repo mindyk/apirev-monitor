bae7bf4945a53fd0c005879b939d359505aaa22e
mindyk <michael.indyk@innogames.de>
2013-10-18 19:05:32 +0200

    molding base

--- cli/main.php
+++ cli/main.php
@@ -3,10 +3,19 @@
 /**
  * Entry point for cli
  */
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
+$currentDir = dirname(__FILE__);
+require_once $currentDir . '/../vendor/autoload.php';
 
 use Apirevmonitor\Application;
 
+$rawJsonConfig = file_get_contents($currentDir . '/../etc/config.json.dist');
+$config = json_decode($rawJsonConfig, true);
 $app = new Application();
-$app->add(new Apirevmonitor\Command\CollectData());
+
+$collectDataCmd = new Apirevmonitor\Command\CollectData();
+$collectDataCmd->setCli(new Apirevmonitor\Shell\Proxy());
+$collectDataCmd->setConfig($config);
+
+$app->add($collectDataCmd);
+
 $app->run();
--- src/main/php/Apirevmonitor/Command/CollectData.php
+++ src/main/php/Apirevmonitor/Command/CollectData.php
@@ -2,6 +2,7 @@
 
 namespace Apirevmonitor\Command;
 
+use Apirevmonitor\Shell\Command\Scp;
 use Symfony\Component\Console\Command\Command as ConsoleCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -17,18 +18,54 @@ class CollectData extends ConsoleCommand {
 	 */
 	protected $cli;
 
+	/**
+	 * @var \DateTime $dateTime
+	 */
+	protected $dateTime;
+
+	/**
+	 * @var array
+	 */
+	protected $config;
+
 	protected function configure(){
 		$this->setName('collect-data')
 			->setDescription('gets access.log from api01-04.portal and process it')
 			->setHelp('The <info>collect-data</info> command copys access.log via scp from api01-04.portal and process the data for monitoring');
 	}
 
+	protected function initialize(InputInterface $input, OutputInterface $output) {
+		$this->dateTime = new \DateTime();
+	}
+
 	public function execute(InputInterface $input, OutputInterface $output) {
 		$output->writeln('running ...');
+		$this->cli->setOutput($output);
+		$output->writeln('collecting logs');
+		$this->collectAccessLogs();
+		$output->writeln('... finish');
+	}
+
+	private function collectAccessLogs() {
+		$timestamp = $this->dateTime->getTimestamp();
+		$remotePath = 'www/portal-api/logs/access.log';
+		$remoteFormat = $this->config['api_server_url'];
+		$localFormat = 'tmp/api0%d.portal.access_%s.log';
+		for ($i=1;$i<=4;$i++) {
+			$currentRemotePath = sprintf($remoteFormat, $i, $remotePath);
+			$currentLocalPath = sprintf($localFormat, $i, $timestamp);
+
+			$this->cli->run(new Scp($currentRemotePath, $currentLocalPath));
+		}
 	}
 
 	public function setCli(Proxy $cli) {
 		$this->cli = $cli;
+		$this->cli->setDryRun(true);
+	}
+
+	public function setConfig(array $config) {
+		$this->config = $config;
 	}
 
 }
